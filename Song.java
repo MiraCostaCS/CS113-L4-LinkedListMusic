302dca2769b8377d7d65ed95cbb0f489bab1741b
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 17:37:14 -0700

    Create Song class with attributes and methods
    Added a class definition for Song with attributes and methods.

---
+++ Song.java
@@ -0,0 +1,19 @@
+// class Song 
+    // -title:String
+    // -artist:String
+    // -duration:double
+
+    // Full Constructor
+
+
+    // All three Getters
+
+
+    // toString method to display song details
+    /************ Uncomment this
+    @Override
+    public String toString() {
+        return "Song: " + title + " by " + artist + " (" + duration + " minutes)";
+    }
+    ****************/
+//}
